06569aecdd5a62d8de6f8f030346fed2bd9d7b94
TheCodingPantsula <asmdigital14@gmail.com>
2023-12-19 02:38:59 +0200

    saving my current work

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -9,27 +9,33 @@ import { MembersModal } from "../modals/members-modal";
 import { CreateChannelModal } from "../modals/create-channel-modal";
 import { LeaveServerModal } from "../modals/leave-server-modal";
 import { DeleteServerModal } from "../modals/delete-server-modal";
+import { DeleteChannelModal } from "../modals/delete-channel-modal";
+import { EditChannelModal } from "../modals/edit-channel-modal";
+import { MessageFileModal } from "../modals/message-file-modal";
 
 export const ModalProvider = () => {
-    const [isMounted, setIsMounted] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    if (!isMounted) {
-        return null;
-    }
+  if (!isMounted) {
+    return null;
+  }
 
-    return (
-        <>
-            <CreateServerModal />
-            <InviteModal />
-            <EditServerModal />
-            <MembersModal />
-            <CreateChannelModal />
-            <LeaveServerModal />
-            <DeleteServerModal />
-        </>
-    )
-}
+  return (
+    <>
+      <CreateServerModal />
+      <InviteModal />
+      <EditServerModal />
+      <MembersModal />
+      <CreateChannelModal />
+      <LeaveServerModal />
+      <DeleteServerModal />
+      <DeleteChannelModal />
+      <EditChannelModal />
+      <MessageFileModal />
+    </>
+  );
+};
--- types.ts
+++ types.ts
@@ -1,6 +1,16 @@
-import { Server, Member, Porfile } from "@prisma/client"
-
+import { Server as NetServer, Socket } from "net";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Server as SocketIOServer } from "socket.io";
+import { Server, Member, Porfile } from "@prisma/client";
 
 export type ServerWithMembersWithProfiles = Server & {
-    members: (Member & { profile: Porfile })[];
+  members: (Member & { profile: Porfile })[];
+};
+
+export type NextApiResponseServerIo = NextApiResponse & {
+  socket: Socket & {
+    server: NetServer & {
+      io: SocketIOServer;
+    };
+  };
 };
